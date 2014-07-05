8c42fec87d76dbf83a79ac58d45d9ca80bc56ff8
Sebastian Hagedorn <shagedorn.dd@gmail.com>
2014-07-05 21:54:23 +0200

    Initial Commit

---
+++ StateRestorationDemo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  StateRestorationDemo
+//
+//  Created by Sebastian Hagedorn on 05/07/14.
+//  Copyright (c) 2014 Sebastian Hagedorn. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
